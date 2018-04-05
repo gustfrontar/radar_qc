6358842eb509923cf3cd779d72ef0c3647d882b6
gustfrontar <gustfrontar@gmail.com>
2018-04-05 20:11:17 -0300

    Bug fixes

---
+++ conf.py
@@ -0,0 +1,105 @@
+#print __doc__
+
+# Author: Rapid Refresh Argentina Team
+# License: BSD 3 clause
+
+# History:  Created 10-2017
+
+#Add path to additional modules.
+
+#======================================
+# ENVIRONMENTAL VARIABLES 
+#======================================
+
+import os
+
+os.environ['OMP_STACKSIZE'] = "1G" # visible in this process + all children
+
+#======================================
+# QC PARAMETERS
+#======================================
+options = {}
+
+#General
+
+options['ref_name']='dBZ'            #Reflectivity
+options['cref_name']='CdBZ'          #Corrected reflectivity (qc output)
+options['v_name']='V'                #Dopper velocity
+options['cv_name']='CV'              #Corrected wind (qc ouput)
+options['rho_name']='RhoHV'          #Rho HV
+
+options['norainrefval']=-0.1
+options['undef']=-9.99e9
+
+#Dealiasing parameters (pyart)
+options['ifdealias']=False
+
+options['interval_split']=3
+options['skip_between_rays']=10
+options['skip_along_rays']=10
+
+#Rho filter parameters
+options['ifrhofilter']=False    #Rhohv filter
+
+options['rhofilternx']=2
+options['rhofilterny']=2
+options['rhofilternz']=0
+options['rhofiltertr']=0.5
+options['rhofilter_save']=False
+
+#Echo top parameters           #Filter layeers with echo top below a certain threshold.
+options['ifetfilter']=False      #Echo top filter
+
+options['etfilternx']=2        #Smooth parameter in x
+options['etfilterny']=2        #Smooth parameter in y
+options['etfilternz']=0        #Smooth parameter in z (dummy)
+options['etfiltertr']=3000     #Echo top threshold.
+options['etfilter_save']=True  #Wether echo top will be included in the output
+
+#Echo depth parameters          #Filters layers with depths below a certain threshold.
+options['ifedfilter']=False     #Echo depth filter
+
+options['edfilternx']=2         #Smooth parameter in x
+options['edfilterny']=2         #Smooth parameter in y
+options['edfilternz']=0         #Smooth parameter in z (dummy)
+options['edfiltertr']=3000      #Echo top threshold.
+options['edfilter_save']=True   #Wether echo top will be included in the output
+
+#Speckle parameters
+options['ifspfilter']=False     #Speckle filter
+
+options['spfilternx']=2           #Box size in X NX=(2*spfilternx + 1)
+options['spfilterny']=2           #Box size in Y
+options['spfilternz']=0           #Box size in Z
+options['spfilterreftr']=5        #Reflectivity threshold
+options['spfiltertr']=0.3         #Count threshold
+options['spfilter_save']=True     #Save filter fields.
+options['spfilter_ref']=True      #Wether the filter will be applied to Ref.
+options['spfilter_v']=True        #Wether the filter will be applied to Vr
+
+#Attenuation parameters
+options['ifattfilter']=False    #Attenuation filter
+
+options['attfiltertr']=20.0       #Attenuation threshold in dBZ
+options['attcalerror']=1.0        #Calibration error
+options['attfilter_save']=True    #Save filter fields
+
+#Blocking parameters
+options['ifblfilter']=True      #Blocking filter
+
+options['blocking_correction']=True  #Wether correction will be applied for partially blocked beams.
+options['blocking_threshold']=0.5    #Beams with blocking above this threshold will be eliminated.
+options['blocking_save']=True        #Save blocking factor into qc_output dictionary.
+
+#Detect missing parameters
+options['ifmissfilter']=False   #Missing values filter
+
+
+#Topography parameters
+
+options['toporawdatapath']="/home/jruiz/share/radar_qc_da/data/terrain_data/raw/"
+options['toporadardatapath']="/home/jruiz/share/radar_qc_da/data/terrain_data/radar/"
+
+
+
+
---
+++ test_radar_qc_ypy.py
@@ -0,0 +1,37 @@
+#print __doc__
+
+# Author: Rapid Refresh Argentina Team
+# License: BSD 3 clause
+
+# History:  Created 10-2017
+
+#Add path to additional modules.
+import sys
+sys.path.append('./src/python' )
+sys.path.append('./src/fortran')
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pyart
+import netCDF4
+import radar_qc_module as rqc
+import os
+
+import conf
+
+#=======================================
+
+
+# read in the file, create a RadarMapDisplay object
+filename = './cfrad.20091117_174348.000_to_20091117_174737.000_PAR_SUR.nc'
+
+#Performs QC operations based on options
+[radar , qc_output] = rqc.main_qc( filename , conf.options )
+
+print('End of QC')
+
+plt.figure()
+plt.pcolor(qc_output['x'][:,:,0],qc_output['y'][:,:,0],qc_output['blocking'][:,:,0])
+plt.colorbar()
+plt.show()
+
