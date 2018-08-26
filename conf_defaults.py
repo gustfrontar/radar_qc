Several bug fixes. Fixes associated with echo top. Fixes associated with numerical precission Fixes associated with order variable.

conf_defaults.py
@@ -21,10 +21,13 @@ options = {}
 #======================================
 
 options['filename'] = ''          #The name of the input netcdf file
-options['output_to_file'] = True #Wether the output will be written to a netcdf file.
+options['output_to_file'] = False #Wether the output will be written to a netcdf file.
 options['filename_out'] = ''      #The name of the netcdf file where output will be written.
 options['file_out_format']='NETCDF4'
 options['keep_original_fields']=False
+#Topography database paths
+options['toporawdatapath']=""
+options['toporadardatapath']=""
 
 #======================================
 # PLOTING PARAMETERS
@@ -36,10 +39,10 @@ options['plot']['Path']='./'
 options['plot']['FigNameSufix']='.png'
 options['plot']['VrMin']=-30
 options['plot']['VrMax']=30
-options['plot']['DbzMin']=0
+options['plot']['DbzMin']=-30
 options['plot']['DbzMax']=70
 options['plot']['Elevs']=[0]             #List with elevations that will be ploted.
-options['plot']['Show']=False
+options['plot']['Show']=True
 options['plot']['CmapWind']='pyart_NWSVel'
 options['plot']['CmapDbz']='pyart_NWSRef'
 
@@ -152,7 +155,7 @@ options[filter_name]['flag']=True                          #Enable / disable fil
 options[filter_name]['nx']=2                                #NX
 options[filter_name]['ny']=2                                #NY
 options[filter_name]['nz']=0                                #NZ
-options[filter_name]['fast_computation']=True               #Enable fast version of echo top computation.
+options[filter_name]['fast_computation']=False               #Enable fast version of echo top computation.
 options[filter_name]['save']=False                          #Save filter aux fields to output?
 options[filter_name]['ify']=np.array([1,1,0,0])             #Importance function y
 options[filter_name]['ifx']=np.array([0,2500,3000,20000])   #Importance function x
@@ -192,7 +195,7 @@ options[filter_name]['fill_value']='undef'                  #Possible values, un
 
 filter_name='RefSpeckleFilter'
 options[filter_name]=dict()
-options[filter_name]['flag']=True                          #Enable / disable filter
+options[filter_name]['flag']=False                          #Enable / disable filter
 options[filter_name]['nx']=2                                #NX
 options[filter_name]['ny']=2                                #NY
 options[filter_name]['nz']=0                                #NZ
@@ -275,7 +278,7 @@ options[filter_name]['fill_value']='undef'                  #Possible values, un
 #Attenuation parameters           ==============================================================
 filter_name='AttenuationFilter'
 options[filter_name]=dict()
-options[filter_name]['flag']=True                           #Enable / Disable filter
+options[filter_name]['flag']=False                           #Enable / Disable filter
 options[filter_name]['nx']=0                                #NX
 options[filter_name]['ny']=0                                #NY
 options[filter_name]['nz']=0                                #NZ
@@ -307,7 +310,7 @@ options[filter_name]['fill_value']='undef'                  #Possible values, un
 #This filter is not part of the Fuzzy logic algorithm.
 filter_name='BlockingFilter'
 options[filter_name]=dict()
-options[filter_name]['flag']=True                           #Enable / Disable filter
+options[filter_name]['flag']=False                           #Enable / Disable filter
 options[filter_name]['blocking_correction']=True            #Wether correction will be applied for partially blocked beams.
 options[filter_name]['save']=False                          #Save blocking factor into qc_output dictionary.
 options[filter_name]['code']=40                             #QC output code
@@ -346,7 +349,7 @@ options[filter_name]['fill_value']='undef'                  #Possible values, un
 #Low doppler velocity filter            ==============================================================
 filter_name='LowDopplerFilter'
 options[filter_name]=dict()
-options[filter_name]['flag']=True                          #Enable / Disable filter
+options[filter_name]['flag']=False                          #Enable / Disable filter
 options[filter_name]['nx']=0                                #NX
 options[filter_name]['ny']=0                                #NY
 options[filter_name]['nz']=0                                #NZ
@@ -367,7 +370,7 @@ options[filter_name]['fill_value']='undef'                  #Possible values, un
 #This filter is not included in the Fuzzy-logic approach.
 filter_name='InterferenceFilter'
 options[filter_name]=dict()
-options[filter_name]['flag']=True                          #Enable / Disable filter
+options[filter_name]['flag']=False                          #Enable / Disable filter
 options[filter_name]['save']=False                          #Save filter aux fields to output?
 options[filter_name]['nx']=0                                #NX
 options[filter_name]['ny']=4                                #NY
@@ -382,7 +385,7 @@ options[filter_name]['Smooth_Ref']=True                     #Smooth reflectivity
 options[filter_name]['Power_Regression']=True               #Wether robust regression will be performed in Dbz scale or linear scale
 options[filter_name]['offset']=200                          #Number of pixels from radar location that will be ignored in the regression.
 options[filter_name]['AzimuthFilter']=True                  #Enable filter to remove isolated pixels in azimuth.
-options[filter_name]['ElevationFilter']=False                #Enable filter to remove isolated pixels in elevation.
+options[filter_name]['ElevationFilter']=False               #Enable filter to remove isolated pixels in elevation.
 options[filter_name]['npass_filter']=3                      #Number of passes of the azimuthal continuity filter.
 options[filter_name]['percent_valid_threshold']=0.1         #Rays with valid pixels over this percentaje will be examinated.
 options[filter_name]['corr_threshold']=0.4                  #Rays that correlates well with the interference pattern will be flagged as 
@@ -491,7 +494,7 @@ options[filter_name]['fill_value']='undef'                  #Possible values, un
 #Detects holes in high reflectivity regions. 
 filter_name='MissingRefFilter'
 options[filter_name]=dict()
-options[filter_name]['flag']=True                           #Enable / Disable filter
+options[filter_name]['flag']=False                           #Enable / Disable filter
 options[filter_name]['threshold']=10                        #Threshold to detect sudden jumps in reflectivity between two consecutive pixels.
 options[filter_name]['nmissing_max']=15                     #Maximum number of missing values in a radial beam.
 options[filter_name]['save']=False                          #Save filter aux fields to output?
@@ -505,16 +508,5 @@ options[filter_name]['order'] = [50]
 options[filter_name]['var_update_list']=['ref']             #Which variables will be filtered.
 options[filter_name]['sequential']=True                     #Wheter this filter will affect the following  filters
 options[filter_name]['fill_value']='undef'                  #Possible values, undef, min_ref or fill value
-
-#Topography parameters
-
-#options['toporawdatapath']="./data/terrain_data/raw/"
-#options['toporadardatapath']="./data/terrain_data/radar/"
-
-options['toporawdatapath']="/media/jruiz/PAWR/Dropbox/DATA/radar_qc/data/terrain_data/raw/"
-options['toporadardatapath']="/media/jruiz/PAWR/Dropbox/DATA/radar_qc/data/terrain_data/radar/"
-
-#options['toporawdatapath']="/home/jruiz/Dropbox/DATA/radar_qc/data/terrain_data/raw/"
-#options['toporadardatapath']="/home/jruiz/Dropbox/DATA/radar_qc/data/terrain_data/radar/"
 
 

src/python/operational_tools.py
@@ -1,23 +1,24 @@
+#print __doc__
+# Author: Rapid Refresh Argentina Team
+# License: BSD 3 clause
 
 
-def read_multiple_files(  filelist , instrument_list = None )
+def read_multiple_files(  file_list , instrument_list = None )          :
 
    import numpy as np
-   #filelist is a list of filenames corresponding to radar files in different formats.
+   import datetime as dt
+   #file_list is a list of filenames corresponding to radar files in different formats.
    #instrument_list (if present) a list of instrument names. Only names in instrument_list will be incorporated to the 
    #radar object list.
 
    #Generate a list of radar objects
+   dtype = 'float64'  #On output radar object fields will have this precission.
    
-   radar_list   = [] 
-   radar_used   = []
-   radar_remove = []
-   
-   final_radar_list = []
+   radar_list   = []                                       #Final list of radar objects.
 
-   used_file = np.zeros( len( filelist ) ).astype(bool)
+   used_file = np.zeros( len( file_list ) ).astype(bool)    #Keep record of which files has been used
 
-   for ifile , filename in enumerate( filelist )    :
+   for ifile , filename in enumerate( file_list )    :
 
       file_format = get_file_format( filename )
  
@@ -29,56 +30,56 @@ def read_multiple_files(  filelist , instrument_list = None )
          #Read the radar 
          my_radar = read_file( filename , file_format )  
 
-         if len( tmp_radar ) != 0  :
+         if my_radar != None  :
             #Add the current radar to the list. 
             #radar_list.append = my_radar
-            radar_used[ifile] = True
             my_radar.files = [ filename ]
+            used_file[ ifile ] = True
 
             #Check if we can associate other radars in the list to the current radar.
   
             for tmp_ifile , tmp_filename  in enumerate( file_list )   :
   
-               if ( not radar_used[tmp_ifile] ) and ( tmp_ifile != ifile )       :
-                  if ( file_format     ==  get_file_format( tmp_filename ) ) or 
-                     ( file_time       ==  get_time_from_filename( tmp_filename ) ) or
-                     ( file_instrument ==  get_instrument_type_from_filename( tmp_filename ) )
+               if ( not used_file[tmp_ifile] )    :
+                  if ( ( file_format     ==  get_file_format( tmp_filename ) ) and 
+                       ( file_time       ==  get_time_from_filename( tmp_filename ) ) and
+                       ( file_instrument ==  get_instrument_type_from_filename( tmp_filename ) ) )  :
 
                      #Read the data
                      tmp_radar = read_file( filename , file_format )
-                     if len( tmp_radar ) != 0  :
+                     if tmp_radar !=  None  :
                         
-                        #Check if we can merge current_radar and my_radar objects.
-                        if np.shape( my_radar.azimuth['data'] )[0] == np.shape( tmp_radar.azimuth['data'] )[0]   :
-                           if np.sum( my_radar.azimuth['data'] - tmp_radar.azimuth['data'] ) == 0                :
-                            #my_radar corresponds to the same instrumet and initial time as current_radar
-                            #merge tmp_radar into my_radar
-                            my_radar.files.append( tmp_filename )
-                            used_file[ tmp_ifile ] = True
-                            for my_key in my_radar.fields   :
-                               if not my_key in my_radar.fields   :
-                                  my_radar.fields[my_key] = tmp_radar.fields.pop(my_key)
-                        else                                                                                     :
+                        #Check if we can merge tmp_radar and my_radar objects.
+                        [ my_radar , merged ] = merge_radar_object( my_radar , tmp_radar )
+                        if merged   :
+                           my_radar.files.append( tmp_filename )
+                           used_file[ tmp_ifile ] = True
+                           
+                        else        :       
                            print('Warning: Inconsistent shapes found for ' + file_instrument + ' ' + file_time )
                            print('This volume will be processed separately')  
 
-         #So far my_radar contains all the variables corresponding to this instrument and initial time.
-         radar_list.append( my_radar )       
+            #So far my_radar contains all the variables corresponding to this instrument and initial time.
+            my_radar = get_strat( filename , my_radar )  #Additional metadata that will be required by QC
+            my_radar = rename_fields( my_radar )         #Rename fields different instruments share the same name convention.
+            my_radar = change_precission( my_radar , dtype )
+          
+            radar_list.append( my_radar )       
 
-         print('RADAR : ' + my_radar.metadata['instrument_name'] + ' ' + my_radar.metadata['start_datetime'] )
-         for ifile in my_radar.files   :
-            print('   FILE: ' + ifile )
+            print('RADAR : ' + my_radar.metadata['instrument_name'] + ' ' + file_time.strftime('%Y-%m-%d-%H-%M-%S') )
+            for ifile in my_radar.files   :
+                print('   FILE: ' + ifile )
          
    return radar_list
 
 def get_file_format( filename )            :
 
-      if ('.h5' in filename ) or ( '.H5' in filename )   :
-         file_format = 'h5'
-      if ( '.vol' in filename ) or ( 'VOL' in filename ) :
-         file_format = 'vol' 
-      if ( '.nc'  in filename ) or ( 'NC' in filename )  :
-         file_format = 'nc' 
+   if ('.h5' in filename ) or ( '.H5' in filename )   :
+      file_format = 'h5'
+   if ( '.vol' in filename ) or ( 'VOL' in filename ) :
+      file_format = 'vol' 
+   if ( '.nc'  in filename ) or ( 'NC' in filename )  :
+      file_format = 'nc' 
 
    return file_format
 
@@ -86,6 +87,8 @@ def read_file( filename , format_file )    :
    import pyart
    from pyart.aux_io.sinarame_h5 import read_sinarame_h5
    from pyart.aux_io.rainbow_wrl import read_rainbow_wrl
+
+   radar = None
 
 
    try   :
@@ -96,35 +99,36 @@ def read_file( filename , format_file )    :
       if format_file == 'nc'   :
          radar = pyart.io.read(filename)
 
+      print( ' ' )
+      print( '=============================================================================')
       print( 'Reading file:' + filename )
-      for my_key in radar.fields         
+      print( '=============================================================================')
+      print( ' ' )
+      for my_key in radar.fields   :    
          print( 'Found the following variable: ' + my_key )
-
-   except ValueError   :
-         print('Warning: Could not read file ' + filename )
-         radar = dict()
-   except KeyError     :
-         print('Warning: Could not read file ' + filename )
-         radar = dict()
+      print( ' ' )
+   except  :
+      print('Warning: Could not read file ' + filename )
+      radar = None
 
    return radar
 
 def rename_fields ( radar )  :
 
-     # Reflectividad RMA
-     if 'TH' in radar.fields
-        radar.fields['ZH'] = radar.fields.pop('TH')
+  # Reflectividad RMA
+  if 'TH' in radar.fields    :
+     radar.fields['ZH'] = radar.fields.pop('TH')
 
-     if 'V' in radar.fields
-        radar.fields['VRAD'] = radar.fields.pop('V')
+  if 'V' in radar.fields     :
+     radar.fields['VRAD'] = radar.fields.pop('V')
 
-     if 'ZH' in radar.fields
-        radar.fields['ZH'] = radar.fields.pop('dBZ')
+  if 'dBZ' in radar.fields    :
+     radar.fields['ZH'] = radar.fields.pop('dBZ')
 
-     if 'W' in radar.fields
-        radar.fields['WRAD'] = radar.fields.pop('W')
+  if 'W' in radar.fields     :
+     radar.fields['WRAD'] = radar.fields.pop('W')
 
-   return radar   
+  return radar   
 
 def get_strat ( filename , radar )  :
 
@@ -138,7 +142,7 @@ def get_strat ( filename , radar )  :
     levels=np.unique(radar.elevation['data'])
 
     #Set the instrument name 
-    radar.metadata['instrument_name'] = get_instrument_name_from_filename( filename )
+    radar.metadata['instrument_name'] = get_instrument_type_from_filename( filename )
 
     #Add missing structures to the radar object.
     if radar.altitude_agl == None :
@@ -317,7 +321,7 @@ def get_strat ( filename , radar )  :
     return radar
 
 
-def get_file_list( datapath , init_time , end_time , time_search_type = None , file_types_list = None )
+def get_file_list( datapath , init_time , end_time , time_search_type = None , file_type_list = None )     :
 
    #datapath : base path of radar data
    #init time: [yyyymmddhhMMss] beginning of the time window
@@ -332,27 +336,27 @@ def get_file_list( datapath , init_time , end_time , time_search_type = None , f
    if time_search_type == None :
       time_search_type = 'timestamp'
 
-   date_min = datetime.strptime( init_time , '%Y%m%d%H%M%S')
-   date_max = datetime.strptime( end_time  , '%Y%m%d%H%M%S')
+   date_min = dt.datetime.strptime( init_time , '%Y%m%d%H%M%S')
+   date_max = dt.datetime.strptime( end_time  , '%Y%m%d%H%M%S')
 
    file_list=[]
 
-      for (dirpath, dirnames, filenames) in os.walk( datapath ):
+   for (dirpath, dirnames, filenames) in os.walk( datapath ):
 
-         for filename in filenames            :
-            f = '/'.join([dirpath,filename])
+      for filename in filenames            :
+         f = '/'.join([dirpath,filename])
 
-            if time_search_type == 'filename'   :
-               date_c = get_time_from_filename( filename )
-            if time_search_type == 'timestamp'  :
-               date_c = dt.fromtimestamp( os.stat(f).st_ctime )
+         if time_search_type == 'filename'   :
+            date_c = get_time_from_filename( filename )
+         if time_search_type == 'timestamp'  :
+            date_c = dt.fromtimestamp( os.stat(f).st_ctime )
 
-            if date_c >= date_min and date_c <= date_max  :
-               file_list.append(f)
+         if date_c >= date_min and date_c <= date_max  :
+            file_list.append(f)
    
    #Keep only some file names and some paths.
 
-   final_file_list[]
+   final_file_list = []
 
    if file_type_list != None :
 
@@ -369,11 +373,11 @@ def get_file_list( datapath , init_time , end_time , time_search_type = None , f
       final_flie_list  = file_list 
 
 
-return file_list
+   return file_list
 
 
 
-def get_time_from_file_name( filename )    :
+def get_time_from_filename( filename )    :
 
    import datetime as dt
    import os 
@@ -382,15 +386,15 @@ def get_time_from_file_name( filename )    :
    file_time = None
 
    if ( 'PAR' in filename ) or ( 'PER' in filename ) or ( 'ANG' in filename )  :
-      file_time  = datetime.strptime(filename[:14], '%Y%m%d%H%M%S')
+      file_time  = dt.datetime.strptime(filename[:14], '%Y%m%d%H%M%S')
 
    if ( 'RMA' in filename )  :
-      file_time  = datetime.strptime(filename.split('_')[-1][:15], '%Y%m%dT%H%M%S')  
+      file_time  = dt.datetime.strptime(filename.split('_')[-1][:15], '%Y%m%dT%H%M%S')  
 
-return file_time
+   return file_time
 
 
-def get_instrument_name_from_file_name( filename ) :
+def get_instrument_type_from_filename( filename ) :
    import os
 
    filename = os.path.basename( filename )
@@ -403,4 +407,134 @@ def get_instrument_name_from_file_name( filename ) :
       instrument_name = 'PAR'
    if 'PER' in filename    :
       instrument_name = 'PER'
+
+   return instrument_name    
+
+
+
+def merge_radar_object( radar_1 , radar_2 )    :
+
+   import numpy as np
+
+   na_1 = np.shape( radar_1.azimuth['data'] )[0]
+   na_2 = np.shape( radar_2.azimuth['data'] )[0]
+
+   nr_1 = np.shape( radar_1.range['data'] )[0]
+   nr_2 = np.shape( radar_2.range['data'] )[0]
+
+
+   azimuth_1 = radar_1.azimuth['data'] 
+   azimuth_2 = radar_2.azimuth['data']
+
+   range_1   = radar_1.range['data']
+   range_2   = radar_2.range['data']
+
+   elev_1    = radar_1.elevation['data']
+   elev_2    = radar_2.elevation['data']
+
+   merged = False  #Wether the two radars has been successfully merged.
+
+   if na_1 == na_2  :
+      diff_a = np.sum( azimuth_1 - azimuth_2 )
+   else             :
+      diff_a = 0.0
+   if nr_1 == nr_2  :
+      diff_r = np.sum( range_1   - range_2   )
+   else             :
+      diff_r = 0.0
+
+   #Check the ideal case
+   if ( na_1 == na_2 ) and ( nr_1 == nr_2 ) and ( diff_a == 0.0 ) and ( diff_r == 0.0 )  :
+      #Dimensions of radar_1 and radar_2 are the same.
+      for my_key in radar_2.fields   :
+         if not my_key in radar_1.fields   :
+            radar_1.fields[my_key] = radar_2.fields.pop(my_key)
+            merged = True
+   else                                                                                  :
+      print('Warning: Inconsistent shapes found for ' + file_instrument + ' ' + file_time )
+      #Test if radial shapes conform.
+      if  nr_1 != nr_2  :
+         #These objects have different ranges. We will try to solve this issue.
+         if nr_1 < nr_2 :
+            small_range = range_1
+            big_range   = range_2
+            small_radar = radar_1
+            big_radar   = radar_2
+            small_na    = na_1
+            big_na      = na_2
+            small_nr    = nr_1
+            big_nr      = nr_2
+         else           :
+            small_range = range_2
+            big_range   = range_1
+            big_radar   = radar_1
+            small_radar = radar_2
+            small_na    = na_2
+            big_na      = na_1
+            small_nr    = nr_2
+            big_nr      = nr_1
+
+         if np.shape( np.intersect1d( small_range , big_range ) )[0] == np.shape( small_range )[0]  :
+            #We take the filds corresponding to the small_radar and we extend them so they have the same
+            #number of ranges as the large_radar.
+            for my_key in small_radar.fields    :
+                if not my_key in big_radar.fields   :
+                    tmp_field = small_radar.fields[ my_key ] 
+                    undef = tmp_field['_FillValue']
+                    tmp_data = np.ones( ( small_na , big_nr ) ) * undef 
+                    tmp_data[ : , 0:small_nr ] = tmp_field['data'].data
+                    small_radar.fields[ my_key ]['data'] = np.ma.masked_array( tmp_data , tmp_data == undef )
+                    small_nr = big_nr
+         else                                                                                        :
+             merged = False
+             return radar_1 , merged 
+
+      #At this point nr is the same for both radars. Before combining the radars we need to check if 
+      #azimuths can be combined. 
+      if ( na_1 == na_2 ) and ( diff_a == 0.0 )   :
+         for my_key in radar_2.fields    :
+            if not my_key in radar_1.fields   :
+               radar_1.fields[my_key] = radar_2.fields.pop(my_key)
+               merged = True
+
+      if ( na_1 != na_2 )   :
+         #Azimuths differ. We will take azimuths_1 as a reference and try to make them compatible.
+         if ( np.shape( np.intersect1d( azimuth_1 , azimuth_2 ) )[0] / na_1 ) >= 0.95    :
+            #Both objects are similar we will try to merge them.
+            for my_key in radar_2.fields    :
+               if not my_key in radar_1.fields   :
+                   undef = radar_1.fields[ my_key ][ '_FillValue' ] 
+                   tmp_data = np.ones( ( na_1 , nr_1 ) ) * undef 
+                   i2=0
+                   for i1 in range( 0 , na_1 )   :
+                       if( azimuth_1[i1] == azimuth_2[i2] ) and ( elevation_1[i1] == elevation_2[i2] ) :
+                           #We have a match.
+                           tmp_data[i1] = radar_2.fields[ my_key ]['data'][i2]
+                           i2 = i2 + 1
+                       if( azimuth_1[i1] < azimuth_2[i2] ) and ( elevation_1[i1] <= elevation_2[i2] )  :
+                           i2 = i2 + 1
+                       if( elevation_1[i1] < elevation_2[i2]  )                                        :
+                           i2 = i2 + 1
+                   radar_1.fields[my_key] = radar_2.fields.pop(my_key)
+                   radar_1.fields[my_key] = np.ma.masked_array( tmp_data , tmp_data == undef )
+            merged = True
+
+
+   return radar_1  , merged
+
+def change_precission( radar , dtype )   :
+   #Some radar objects might have float 32 precission 
+   #while QC routines are expecting float 64 fields.
+
+   import numpy as np
+
+   for my_key in radar.fields  :
+
+       radar.fields[my_key]['data'] = ( radar.fields[my_key]['data'] ).astype( dtype )
+
+
+
+   return radar
+
+
 

src/python/radar_qc_module.py
@@ -2,7 +2,7 @@
 # Author: Rapid Refresh Argentina Team
 # License: BSD 3 clause
 
-def main_qc( options ) :
+def main_qc( options , radar = None ) :
 
    import sys
    import time
@@ -23,17 +23,16 @@ def main_qc( options ) :
 
    #Read the data
 
-   print('')
-   print('-------------------------------------------')
-   print('Reading the data')
-   print('-------------------------------------------')
-   print('')
+   if radar == None  :
 
-   radar = pyart.io.read(options['filename'])
+      print('')
+      print('-------------------------------------------')
+      print('Reading the data')
+      print('-------------------------------------------')
+      print('')
 
-   if options['is_rma']  :
+      radar = pyart.io.read(options['filename'])
 
-      radar = get_rma_strat( options['filename'] , radar ) 
 
    if radar.altitude_agl['data'] == 0.0    :
       #Radar is assumed to be at ground level. Add the height of the tower.
@@ -54,6 +53,18 @@ def main_qc( options ) :
       print('-------------------------------------------')
       print('')
       return radar , output
+
+   #===================================================
+   # CHECK IF WE HAVE INFINITE OR NAN VALUES IN DATA 
+   #===================================================
+
+   print('')
+   print('-------------------------------------------')
+   print('Checking if input data is valid')
+   print('-------------------------------------------')
+   print('')
+
+   radar = check_valid_data( radar , options )
 
    #===================================================
    # SOME PRELIMINARY PROCEDURES.
@@ -140,6 +151,31 @@ def main_qc( options ) :
 # FUNCIONES GENERALES ------------------------------
 #===================================================
 
+#===================================================
+# Check if input data is valid.
+#===================================================
+
+def check_valid_data( radar , options )   :
+
+    import numpy as np
+    import time
+
+    start=time.time()
+
+    for my_key in radar.fields    :
+       if my_key == options['name_ref']   :
+          radar.fields[ my_key ]['data'].data[ np.isinf( radar.fields[ my_key ]['data'].data )  ] = options['norainrefval']
+          radar.fields[ my_key ]['data'].data[ np.isnan( radar.fields[ my_key ]['data'].data )  ] = options['norainrefval']
+          radar.fields[ my_key ]['data'].data[ radar.fields[ my_key ]['data'].data < options['norainrefval']  ] = options['norainrefval']
+          
+       else                               :
+          radar.fields[ my_key ]['data'].data[ np.isinf( radar.fields[ my_key ]['data'].data )  ] = radar.fields[ my_key ]['_FillValue']
+          radar.fields[ my_key ]['data'].data[ np.isnan( radar.fields[ my_key ]['data'].data )  ] = radar.fields[ my_key ]['_FillValue']
+
+    end=time.time()
+    print("The elapsed time in {:s} is {:2f}".format("Input data check",end-start) )
+
+    return radar
 
 #==================================================a
 #' RESHAPE VARIABLES
@@ -165,6 +201,7 @@ def reshape_variables( radar , output , options )    :
         output['nr']=nr
         output['ne']=ne
 
+        output['cref'] = np.ones( (na,nr,ne))
         output['cref'] = np.copy(output['ref'])           #Initialize the corrected reflectivity array.
         output['ref_input'] = np.copy(output['ref'])      #Initialize the input reflectivity array (for ploting only)
  
@@ -175,6 +212,7 @@ def reshape_variables( radar , output , options )    :
         output['wref']  = np.zeros(output['cref'].shape)  #Set the weigths to 0.
 
         output['elevations']=np.unique(radar.elevation['data'])
+
 
    if options['name_v'] in radar.fields  : 
 
@@ -188,7 +226,8 @@ def reshape_variables( radar , output , options )    :
         output['na']=na
         output['nr']=nr
         output['ne']=ne
- 
+
+        output['cv'] = np.ones((na,nr,ne))
         output['cv'] = np.copy(output['v'])              #Initialize the corrected velocity array
         output['input_v'] = np.copy(output['v'])         #Initialize the input velocity array (for ploting only)
 
@@ -217,9 +256,9 @@ def georeference_data( radar , output , options )    :
 
    #dm is a dummy variable
 
-   [ output['altitude'] , dm , dm , dm , dm ] = order_variable( radar , 'altitude' , options['undef'] )
-   [ output['x']        , dm , dm , dm , dm ] = order_variable( radar , 'x' , options['undef'] ) 
-   [ output['y']        , dm , dm , dm , dm ] = order_variable( radar , 'y' , options['undef'] )
+   [ output['altitude'] , dm , dm , dm , dm ] = order_variable( radar , 'altitude' ,  options['undef'] )
+   [ output['x']        , dm , dm , dm , dm ] = order_variable( radar , 'x' ,  options['undef'] ) 
+   [ output['y']        , dm , dm , dm , dm ] = order_variable( radar , 'y' ,  options['undef'] )
 
 
    #Compute distance to radar for the first azimuth (we assume that distance to radar will be the same.
@@ -383,6 +422,9 @@ def output_update( output , qc_index , options , filter_name )  :
    weigth = qc.multiple_1d_interpolation( field=qc_index , nx=na , ny=nr , nz=ne
                                          , undef=options['undef'] , xx=options[filter_name]['ifx']
                                          , yy=options[filter_name]['ify'] , nxx=np.size(options[filter_name]['ifx']) )
+
+   weigth[ weigth == options['undef'] ] = 0.0
+
 
    if ( 'ref' in options[filter_name]['var_update_list'] ) and ( 'ref' in output )  : 
       #Update reflectivity
@@ -760,6 +802,7 @@ def EchoTopFilter( radar , output , options )   :
 
 
       if  options[filter_name]['fast_computation']          :
+
          [tmp_index,tmp_data_2d]=qc.echo_top_fast(reflectivity=output['ref'],heigth=output['altitude'][0,:,:]
                                                 ,rrange=output['distance'],na=na,nr=nr,ne=ne
                                                 ,undef=output['undef_ref'],nx=nx,ny=ny,nz=nz)
@@ -1371,37 +1414,40 @@ def order_variable ( radar , var_name , undef )  :
 
    na=np.size(order_azimuth)
    ne=np.size(levels)
+   nr=np.size(radar.range['data'].data) 
+
+
+   var = np.ones( (nb,nr) )
 
    if ( var_name == 'altitude' ) :
-      var=np.copy(radar.gate_altitude['data'])
+       var[:]=radar.gate_altitude['data']  
    elif( var_name == 'longitude' ) :
-      var=np.copy(radar.gate_longitude['data'])
+       var[:]=radar.gate_longitude['data']
    elif( var_name == 'latitude'  ) :
-      var=np.copy(radar.gate_latitude['data'])
+       var[:]=radar.gate_latitude['data']
    elif( var_name == 'x' )         :
-      var=np.copy(radar.gate_x['data'])
+       var[:]=radar.gate_x['data']
    elif( var_name == 'y' )         : 
-      var=np.copy(radar.gate_y['data'])
+       var[:]=radar.gate_y['data']
    else  :
-      var=np.copy(radar.fields[var_name]['data'].data)
+       var[:]=radar.fields[var_name]['data'].data
 
-   nr=var.shape[1]
 
    #Allocate arrays
    order_var    =np.zeros((na,nr,ne))
    order_time   =np.zeros((na,ne)) 
    azimuth_exact=np.zeros((na,ne))
-   order_n      =np.zeros((na,nr,ne))
+   order_n      =np.zeros((na,nr,ne),dtype='int')
    
    current_lev = radar.elevation['data'][0]
-   ilev = np.where( levels == current_lev  )[0]  
+   ilev = np.where( levels == current_lev )[0]
 
    for iray in range( 0 , nb )  :   #Loop over all the rays
  
      #Check if we are in the same elevation.
      if  radar.elevation['data'][iray] != current_lev  :
-         ilev=ilev = np.where( levels == current_lev  )[0]
-         current_lev = radar.elevation['data'][iray]  
+         current_lev = radar.elevation['data'][iray]
+         ilev=np.where( levels == current_lev  )[0]
 
      #Compute the corresponding azimuth index.
      az_index = np.round( radar.azimuth['data'][iray] / ray_angle_res ).astype(int)
@@ -1447,7 +1493,7 @@ def order_variable_inv (  radar , var , undef )  :
    current_lev = radar.elevation['data'][0]
    ilev = np.where( levels == current_lev  )[0]
 
-   output_var = np.zeros((nb,nr))
+   output_var = np.zeros((nb,nr) )
    output_var[:] = undef
 
    for iray in range( 0 , nb )  :   #Loop over all the rays
@@ -1467,113 +1513,6 @@ def order_variable_inv (  radar , var , undef )  :
 
    return output_var
 
-
-def get_rma_strat ( filename , radar )  :
-
-    import numpy as np
-    import numpy.ma as ma
-
-    local_fill_value = -9999.0
-    levels=np.unique(radar.elevation['data'])
-
-    #Add missing structures to the radar object.
-    if radar.altitude_agl == None :
-       radar.altitude_agl = dict()
-    if radar.metadata == None :
-       radar.metadata = dict()
-    if radar.instrument_parameters == None :
-       radar.instrument_parameters = dict()
-       radar.instrument_parameters['nyquist_velocity']=dict()
-       radar.instrument_parameters['nyquist_velocity']['long_name']='unambiguous_doppler_velocity'
-       radar.instrument_parameters['nyquist_velocity']['units']='meters per second'
-       radar.instrument_parameters['nyquist_velocity']['_FillValue']= local_fill_value
-       radar.instrument_parameters['nyquist_velocity']['meta_group']='instrument_parameters'
- 
-       radar.instrument_parameters['radar_beam_width_v']=dict()
-       radar.instrument_parameters['radar_beam_width_v']['long_name']='half_power_radar_beam_width_v_channel'
-       radar.instrument_parameters['radar_beam_width_v']['units']='degrees'
-       radar.instrument_parameters['radar_beam_width_v']['_FillValue']= local_fill_value
-       radar.instrument_parameters['radar_beam_width_v']['meta_group']='instrument_parameters'
-
-       radar.instrument_parameters['radar_beam_width_h']=dict()
-       radar.instrument_parameters['radar_beam_width_h']['long_name']='half_power_radar_beam_width_h_channel'
-       radar.instrument_parameters['radar_beam_width_h']['units']='degrees'
-       radar.instrument_parameters['radar_beam_width_h']['_FillValue']= local_fill_value
-       radar.instrument_parameters['radar_beam_width_h']['meta_group']='instrument_parameters'
-
-    if radar.range == None :
-       radar.range = dict()
-    if radar.ray_angle_res == None :
-       radar.ray_angle_res = dict()
-       radar.ray_angle_res['long_name']='angular_resolution_between_rays'
-       radar.ray_angle_res['units']='degrees'
-       radar.ray_angle_res['_FillValue']= local_fill_value
-       
-    #Get the corresponding radar strategy depending on the filename.
-    radar.altitude_agl['data'] = 0.0   
-
-    radar.metadata['instrument_name'] = 'RMA' + filename[ filename.find('RMA') + 3 ]
-
-    if '0117_01' in filename  :  #9005-1 STRATEGY
-       nyquist_velocity     = 6.63
-    if '0117_02' in filename  :  #9005-2 STRATEGY
-       nyquist_velocity     = 33.04
-    if '0117_03' in filename  :  #9005-3 STRATEGY
-       nyquist_velocity     = 3.98
-
-    if '0117_01' in filename  :  #122-1 STRATEGY
-       nyquist_velocity     = 6.63
-    if '0117_02' in filename  :  #122-2 STRATEGY
-       nyquist_velocity     = 13.25
-
-    if '0121_01' in filename  :  #122-1 STRATEGY
-       nyquist_velocity     = 6.63
-    if '0121_02' in filename  :  #122-2 STRATEGY
-       nyquist_velocity     = 13.25
-
-    if '0122_01' in filename  :  #122-1 STRATEGY
-       nyquist_velocity     = 8.28
-    if '0122_02' in filename  :  #122-2 STRATEGY
-       nyquist_velocity     = 39.79
-    if '0122_03' in filename  :  #122-3 STRATEGY
-       nyquist_velocity     = 13.35
-
-    if '0123_01' in filename  :  #123-1 STRATEGY
-       nyquist_velocity     = 8.28
-    if '0123_02' in filename  :  #123-2 STRATEGY
-       nyquist_velocity     = 39.79
-    if '0123_03' in filename  :  #123-3 STRATEGY
-       nyquist_velocity     = 13.25
-    if '0123_04' in filename  :  #123-4 STRATEGY
-       nyquist_velocity     = 8.28
-
-    if '0200_01' in filename  :  #200-1 STRATEGY
-       nyquist_velocity     = 4.42
-    if '0200_02' in filename  :  #200-2 STRATEGY
-       nyquist_velocity     = 13.25
-
-    if '0201_01' in filename  :  #201-1 STRATEGY
-       nyquist_velocity     = 4.42
-    if '0201_02' in filename  :  #201-2 STRATEGY
-       nyquist_velocity     = 13.25
-    if '0201_03' in filename  :  #201-3 STRATEGY
-       nyquist_velocity     = 8.28
-
-    #Some common parameters
-    ray_angle_res        = 1.0 
-    radar_beam_width_h   = 1.0
-    radar_beam_width_v   = 1.0
-    meters_between_gates  = radar.range['data'][1]-radar.range['data'][0]
-
-    #Apply the missing parameters to the radar structure.
-    radar.instrument_parameters['nyquist_velocity']['data'] = ma.array(np.ones( np.shape(radar.azimuth['data']) )*nyquist_velocity , mask = np.zeros( np.shape(radar.azimuth['data']) , dtype=bool ) , fill_value = local_fill_value )
-    radar.instrument_parameters['radar_beam_width_h']['data'] = ma.array( radar_beam_width_h , mask =False , fill_value = local_fill_value )
-    radar.instrument_parameters['radar_beam_width_v']['data'] = ma.array( radar_beam_width_v , mask =False , fill_value = local_fill_value )
-    radar.ray_angle_res['data'] = ma.array( np.ones( np.shape( levels ) )*ray_angle_res , mask = np.zeros( np.shape( levels ) , dtype=bool ) , fill_value = local_fill_value )
-    radar.range['meters_between_gates']= meters_between_gates
-    radar.range['meters_to_center_of_first_gate']= radar.range['data'][0]  #meters_between_gates / 2.0
-
-    return radar
 
 
 def dopplerspatialcoherence_filter( v , undef , my_conf ) : 
@@ -1824,6 +1763,8 @@ def interference_filter ( ref , undef , min_ref , r , my_conf )  :
    #r : radar range (vector)
    #my_conf : configuration for this filter.
 
+   ref[ ref == min_ref ] = undef
+
    offset= my_conf['offset']
 
    na=ref.shape[0]
@@ -1846,7 +1787,7 @@ def interference_filter ( ref , undef , min_ref , r , my_conf )  :
    tmp_z = np.power( 10.0, tmp_ref / 10.0  )
 
    tmp_z[tmp_ref == undef ] = undef
-  
+
    
    #offset=my_conf['offset']
    att=my_conf['att']
@@ -1898,16 +1839,23 @@ def interference_filter ( ref , undef , min_ref , r , my_conf )  :
            else:
 
               powerrayo = np.zeros( nr ) + 20.0 * np.log10( r ) + 2.0 * att * r
+              inlier_mask = np.zeros( np.sum( undef_mask.astype(int) ) ).astype(bool) 
 
            #print( np.shape( powerrayo ),np.shape( local_ref )
 
-           if ( np.sum( inlier_mask.astype(int) ) >= 10 ) & ( np.std(local_ref[undef_mask][inlier_mask]) > 0 ) :
-           
-              corrcoef=np.corrcoef( powerrayo[undef_mask][inlier_mask],local_ref[undef_mask][inlier_mask] )[0,1]
+           if ( np.sum( inlier_mask.astype(int) ) >= 10 )  :
+               if ( np.std(local_ref[undef_mask][inlier_mask]) > 0 ) :
+                  #if k == 0  :
+                  #   print( i,k,np.sum( undef_mask.astype(int) ) , np.shape( inlier_mask ) )
+                  #   local_ref[ local_ref < 0 ] = undef
+                  #   print( local_ref[ undef_mask ] )
+                  corrcoef=np.corrcoef( powerrayo[undef_mask][inlier_mask],local_ref[undef_mask][inlier_mask] )[0,1]
+               else                                                 :
+                  corrcoef = np.array(-1.0)
 
            else                                      :
 
-              corrcoef=-1.0
+              corrcoef=np.array(-1.0)
 
            #if (k == 0) and (i == 348) :
            #  plt.plot(  powerrayo[undef_mask] , local_ref[undef_mask] ,'or')
@@ -1953,6 +1901,7 @@ def interference_filter ( ref , undef , min_ref , r , my_conf )  :
 
    #Additional filter for the remaining echoes
    #consider cyclic boundary conditions in azimuth.
+
    for ifilter in range( npass_filter )  :
 
       for k in range(ne) :
@@ -2097,7 +2046,8 @@ def write_topo( my_topo , my_file )           :
     np.reshape( my_topo['max'].astype('f4') , (nr*na) ).tofile(f)
     np.reshape( my_topo['min'].astype('f4') , (nr*na) ).tofile(f)
     np.reshape( my_topo['number'].astype('f4') , (nr*na) ).tofile(f)
-    np.reshape( (my_topo['range'].data).astype('f4') , (nr*na) ).tofile(f)
+    np.reshape( my_topo['range'].astype('f4') , (nr*na) ).tofile(f)
+    #np.reshape( (my_topo['range'].data).astype('f4') , (nr*na) ).tofile(f)
     np.reshape( my_topo['azimuth'].astype('f4') , (nr*na) ).tofile(f)
     np.reshape( my_topo['latitude'].astype('f4') , (nr*na) ).tofile(f)
     np.reshape( my_topo['longitude'].astype('f4') , (nr*na) ).tofile(f)